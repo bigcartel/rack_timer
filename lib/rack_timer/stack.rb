0d093554c9315b8dd37ac4ab33e3e128a5f091a4
Kelley Reynolds <kelley@insidesystems.net>
2013-02-01 12:47:58 -0500

    Revised the output to be easier to deal with, exclude the base application from the total

--- lib/rack_timer/stack.rb
+++ lib/rack_timer/stack.rb
@@ -13,52 +13,59 @@ module ActionController
       end
 
       def call(env)
-        env = incoming_timestamp(env)
+        # Set the first middleware to be called and init a total_time
+        env["MIDDLEWARE_TOTAL"] = [@app.class.to_s, 0] if !env["MIDDLEWARE_TOTAL"]
+
+        env, incoming_elapsed_time = incoming_timestamp(env)
         status, headers, body = @app.call env
-        env = outgoing_timestamp(env)
+
+        # Determine if we are at the application yet
+        application_middleware = @app.class.to_s == env["MIDDLEWARE_TIMESTAMP"][0] ? true : false
+
+        env, outgoing_elapsed_time = outgoing_timestamp(env)
+
+        # Set our times
+        incoming_elapsed_time, outgoing_elapsed_time = incoming_elapsed_time.to_f, outgoing_elapsed_time.to_f
+        total_time = incoming_elapsed_time + outgoing_elapsed_time
+
+        # Do not add the time for the application, just the middleware layers above it
+        env["MIDDLEWARE_TOTAL"][1] += total_time unless application_middleware
+
+        # Log if it's past our threshold
+        if total_time > LogThreshold
+          Rails.logger.info "[Rack Timer] #{@app.class.to_s.rjust(60)} : #{("%.3f" % total_time).rjust(10)} ms (#{"%.3f" % incoming_elapsed_time} + #{"%.3f" % outgoing_elapsed_time}; #{"%.3f" % env["MIDDLEWARE_TOTAL"][1]})"
+        end
         [status, headers, body]
       end
 
       def incoming_timestamp(env)
+        elapsed_time = nil
         if env.has_key?("MIDDLEWARE_TIMESTAMP") # skip over the first middleware
           elapsed_time = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000 
-          if elapsed_time > LogThreshold # only log if took greater than LogThreshold
-            Rails.logger.info "Rack Timer (incoming) -- #{env["MIDDLEWARE_TIMESTAMP"][0]}: #{elapsed_time} ms"
-          end
         elsif env.has_key?("HTTP_X_REQUEST_START") or env.has_key?("HTTP_X_QUEUE_START")
           # if we are tracking request queuing time via New Relic's suggested header(s),
           # then lets see how much time was spent in the request queue by taking the difference
           # between Time.now from the start of the first piece of middleware
           # prefer HTTP_X_QUEUE_START over HTTP_X_REQUEST_START in case both exist
           queue_start_time = (env["HTTP_X_QUEUE_START"] || env["HTTP_X_REQUEST_START"]).gsub("t=", "").to_i
-          Rails.logger.info "Rack Timer -- Queuing time: #{(Time.now.to_f * 1000000).to_i - queue_start_time} microseconds"
         end
         env["MIDDLEWARE_TIMESTAMP"] = [@app.class.to_s, Time.now]
-        env
+        [env, elapsed_time]
       end
 
       def outgoing_timestamp(env)
+        elapsed_time = nil
         if env.has_key?("MIDDLEWARE_TIMESTAMP")
           elapsed_time = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000
-          if elapsed_time > LogThreshold # only log if took greater than LogThreshold
-            if env["MIDDLEWARE_TIMESTAMP"][0] and env["MIDDLEWARE_TIMESTAMP"][0] == @app.class.to_s
-              # this is the actual elapsed time of the final piece of Middleware (typically routing) AND the actual
-              # application's action
-              Rails.logger.info "Rack Timer (Application Action) -- #{@app.class.to_s}: #{elapsed_time} ms"              
-            else
-              Rails.logger.info "Rack Timer (outgoing) -- #{@app.class.to_s}: #{elapsed_time} ms"
-            end
-          end
         end
         env["MIDDLEWARE_TIMESTAMP"] = [nil, Time.now]
-        env
+        [env, elapsed_time]
       end
-
     end
 
     class Middleware
 
-      # overrding the built-in Middleware.build and adding a RackTimer wrapper class
+      # overriding the built-in Middleware.build and adding a RackTimer wrapper class
       def build(app)
         if block
           RackTimer.new klass.new(app, *build_args, &block)

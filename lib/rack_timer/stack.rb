c3a0d6a372bb9463aac0760a9c82d037596fdd9f
Kelley Reynolds <kelley@insidesystems.net>
2013-02-01 16:08:45 -0500

    Revamped reporting

--- lib/rack_timer/stack.rb
+++ lib/rack_timer/stack.rb
@@ -13,28 +13,19 @@ module ActionController
       end
 
       def call(env)
-        # Set the first middleware to be called and init a total_time
-        env["MIDDLEWARE_TOTAL"] = [@app.class.to_s, 0] if !env["MIDDLEWARE_TOTAL"]
-
-        env, incoming_elapsed_time = incoming_timestamp(env)
-        status, headers, body = @app.call env
-
-        # Determine if we are at the application yet
-        application_middleware = @app.class.to_s == env["MIDDLEWARE_TIMESTAMP"][0] ? true : false
-
-        env, outgoing_elapsed_time = outgoing_timestamp(env)
-
-        # Set our times
-        incoming_elapsed_time, outgoing_elapsed_time = incoming_elapsed_time.to_f, outgoing_elapsed_time.to_f
-        total_time = incoming_elapsed_time + outgoing_elapsed_time
-
-        # Do not add the time for the application, just the middleware layers above it
-        env["MIDDLEWARE_TOTAL"][1] += total_time unless application_middleware
-
-        # Log if it's past our threshold
-        if total_time > LogThreshold
-          Rails.logger.info "[Rack Timer] #{@app.class.to_s.rjust(60)} : #{("%.3f" % total_time).rjust(10)} ms (#{"%.3f" % incoming_elapsed_time} + #{"%.3f" % outgoing_elapsed_time}; #{"%.3f" % env["MIDDLEWARE_TOTAL"][1]})"
+        # Determine if this is our first middlewarefirst_middleware = false
+        if !env["MIDDLEWARE_TIME_HASH"]
+          env["MIDDLEWARE_TIME_HASH"] = {}
+          first_middleware = true
         end
+
+        env = incoming_timestamp(env)
+        status, headers, body = @app.call env
+        env = outgoing_timestamp(env)
+
+        # If it's our first middleware, run the report
+        report(env["MIDDLEWARE_TIME_HASH"]) if first_middleware
+
         [status, headers, body]
       end
 
@@ -42,29 +33,43 @@ module ActionController
         elapsed_time = nil
         if env.has_key?("MIDDLEWARE_TIMESTAMP") # skip over the first middleware
           elapsed_time = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000 
+          env["MIDDLEWARE_TIME_HASH"][env["MIDDLEWARE_TIMESTAMP"][0]] = [elapsed_time.to_f]
         elsif env.has_key?("HTTP_X_REQUEST_START") or env.has_key?("HTTP_X_QUEUE_START")
           # if we are tracking request queuing time via New Relic's suggested header(s),
           # then lets see how much time was spent in the request queue by taking the difference
           # between Time.now from the start of the first piece of middleware
           # prefer HTTP_X_QUEUE_START over HTTP_X_REQUEST_START in case both exist
           queue_start_time = (env["HTTP_X_QUEUE_START"] || env["HTTP_X_REQUEST_START"]).gsub("t=", "").to_i
+#          Rails.logger.info "Rack Timer -- Queuing time: #{(Time.now.to_f * 1000000).to_i - queue_start_time} microseconds"
         end
         env["MIDDLEWARE_TIMESTAMP"] = [@app.class.to_s, Time.now]
-        [env, elapsed_time]
+        env
       end
 
       def outgoing_timestamp(env)
         elapsed_time = nil
         if env.has_key?("MIDDLEWARE_TIMESTAMP")
           elapsed_time = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000
+          if env["MIDDLEWARE_TIMESTAMP"][0] and env["MIDDLEWARE_TIMESTAMP"][0] == @app.class.to_s
+            # this is the actual elapsed time of the final piece of Middleware (typically routing) AND the actual
+            # application's action
+            env["MIDDLEWARE_TIME_HASH"]["Application"] = [elapsed_time.to_f]
+          else
+            env["MIDDLEWARE_TIME_HASH"][@app.class.to_s] << elapsed_time.to_f
+          end
         end
         env["MIDDLEWARE_TIMESTAMP"] = [nil, Time.now]
-        [env, elapsed_time]
+        env
+      end
+
+      def report(hash)
+        hash.each_pair do |middleware, times|
+          Rails.logger.info "[Rack Timer] #{middleware.rjust(56)} #{("%.3f" % times.sum).rjust(10)} ms (#{times.map { |t| "%.3f" % t }.join(" + ")})" if times.sum > LogThreshold
+        end
       end
     end
 
     class Middleware
-
       # overriding the built-in Middleware.build and adding a RackTimer wrapper class
       def build(app)
         if block
@@ -73,7 +78,6 @@ module ActionController
           RackTimer.new klass.new(app, *build_args)
         end
       end
-
     end
 
     # overriding this in order to wrap the incoming app in a RackTimer, which gives us timing on the final
